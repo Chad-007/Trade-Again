ec34edb7fabf13b1769089020f5ff6d39124fbc6
alan <alansebastian484@gmail.com>
2025-09-03 23:23:00 +0530

    first push

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,60 @@
+import express  = require("express")
+import jwt = require("jsonwebtoken")
+const app = express()
+app.use(express.json())
+import Pool  = require("pg")
+import bycrypt  = require("bcrypt")
+import mongoose = require("mongoose")
+
+//@ts-ignore
+const pool = new Pool({
+  host: "127.0.0.1",
+  port: 5432,
+  user: "postgres",
+  password: "alan",
+  database: "postgres"
+});
+
+
+function auth(req:any,res:any,next:any){
+    const authheader = req.headers("Authorization")
+    const token  =  authheader.split(" ")[1];
+    const decoded  = jwt.verify(token,"secretkey") as {id:number,username:string}
+    req.user = decoded
+    next();
+}
+
+app.post("/signup",async(req,res)=>{
+    const{username,password} = req.body
+    try{
+        const hashed = await bycrypt.hash(password,10);
+        const hey = await pool.query("SELECT * from users where username = $1",[username]);
+        if(hey.rows.length===0){
+                await pool.query("insert into users(username,password,balance)values($1,$2)",[username,hashed,10000])
+        }
+        return res.status(200).json({message:"signed up successfully"})
+    }
+    catch(err){
+        return res.status(401).json({message:"there was some issue"})
+    }
+});
+
+app.post("/signin",async(req,res)=>{
+    const{username,password}  = req.body
+    try{
+        const rows  = await pool.query("SELECT * from users where username = $1",username);
+        const user  =  rows.rows[0]
+        const match = bycrypt.compare(password,user.password)
+        if(!match){
+            return res.status(401).json({message:"The password is incorrect"})
+        }
+        const token  = jwt.sign({id:user.id,username:user.username},"secretkey",{expiresIn:"1h"});
+        return res.json({token,username:user.username,balance:user.balance})
+    }catch(err){
+        return res.status(401).json({message:"there was some issue while signin up"})
+    }
+});
+
+// app.post("/placeorder",async(req,res)=>{
+//     const {}
+// })
